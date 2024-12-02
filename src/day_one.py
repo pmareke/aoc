937af99fedd84479701e9e45e033cf205b3f05fe
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-02 08:40:02 +0100

    feat: solve day one

--- src/day_one.py
+++ src/day_one.py
@@ -1,9 +1,40 @@
+from collections import defaultdict
+
+
 class DayOne:
     def __init__(self, input: list[str]) -> None:
         self.input = input
 
     def part_one(self) -> int:
-        return 0
+        left = []
+        right = []
+        for line in self.input:
+            numbers = line.split("   ")
+            left.append(int(numbers[0]))
+            right.append(int(numbers[1]))
+        return self._total_distance(left, right)
+
+    def _total_distance(self, left: list[int], right: list[int]) -> int:
+        left = sorted(left)
+        right = sorted(right)
+
+        result = 0
+        for idx, item in enumerate(left):
+            result += abs(item - right[idx])
+        return result
 
     def part_two(self) -> int:
-        return 0
+        left = []
+        right: dict[int, int] = defaultdict(int)
+        for line in self.input:
+            numbers = line.split("   ")
+            left.append(int(numbers[0]))
+            right[int(numbers[1])] += 1
+
+        return self._calculate_similarity(left, right)
+
+    def _calculate_similarity(self, left: list[int], right: dict[int, int]) -> int:
+        result = 0
+        for idx, item in enumerate(left):
+            result += item * right[item]
+        return result
--- tests/test_day_one.py
+++ tests/test_day_one.py
@@ -7,18 +7,18 @@ from src.day_one import DayOne
 class TestDayOne:
     @pytest.fixture
     def input(self) -> list[str]:
-        return open("inputs/01.in").readlines()
+        return open("inputs/01.in").read().strip().split("\n")
 
     def test_part_one(self, input: list[str]) -> None:
         day_one = DayOne(input)
 
         result = day_one.part_one()
 
-        expect(result).to(equal(0))
+        expect(result).to(equal(3574690))
 
     def test_part_two(self, input: list[str]) -> None:
         day_one = DayOne(input)
 
         result = day_one.part_two()
 
-        expect(result).to(equal(0))
+        expect(result).to(equal(22565391))
