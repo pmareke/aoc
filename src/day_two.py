a552783e4a763b2cf9d5c542206df6d60edd3ea8
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-02 12:34:20 +0100

    feat: remove unused import

--- src/day_two.py
+++ src/day_two.py
@@ -6,60 +6,50 @@ class DayTwo:
         result = 0
         for line in self.input:
             numbers = list(map(int, line.split()))
-
-            desc = sorted(numbers, reverse=True)
-            if numbers == desc:
-                if self._is_safe(desc):
-                    result += 1
-                continue
-
-            asc = sorted(numbers)
-            if numbers == asc:
-                if self._is_safe(asc):
-                    result += 1
-
+            if self._is_safe(numbers):
+                result += 1
         return result
 
     def part_two(self) -> int:
         result = 0
         for line in self.input:
             numbers = list(map(int, line.split()))
-
-            desc = sorted(numbers, reverse=True)
-            if numbers == desc:
-                if self._is_safe(desc):
-                    result += 1
-                    continue
-
-            asc = sorted(numbers)
-            if numbers == asc:
-                if self._is_safe(asc):
-                    result += 1
-                    continue
-
-            valid_numbers = self.remove_one_level(numbers)
-            if valid_numbers:
+            if self._is_safe_v2(numbers):
                 result += 1
-
         return result
 
-    def remove_one_level(self, numbers: list[int]) -> bool:
-        for i in range(len(numbers)):
-            valid_numbers = [*numbers[:i], *numbers[i + 1 :]]
+    def _is_safe(self, numbers: list[int]) -> bool:
+        desc = sorted(numbers, reverse=True)
+        if numbers == desc:
+            if self._is_valid(desc):
+                return True
 
-            desc = sorted(valid_numbers, reverse=True)
-            if valid_numbers == desc:
-                if self._is_safe(desc):
-                    return True
-
-            asc = sorted(valid_numbers)
-            if valid_numbers == asc:
-                if self._is_safe(asc):
-                    return True
+        asc = sorted(numbers)
+        if numbers == asc:
+            if self._is_valid(asc):
+                return True
 
         return False
 
-    def _is_safe(self, numbers: list[int]) -> bool:
+    def _is_safe_v2(self, numbers: list[int]) -> bool:
+        desc = sorted(numbers, reverse=True)
+        if numbers == desc:
+            if self._is_valid(desc):
+                return True
+
+        asc = sorted(numbers)
+        if numbers == asc:
+            if self._is_valid(asc):
+                return True
+
+        for i in range(len(numbers)):
+            valid_numbers = [*numbers[:i], *numbers[i + 1 :]]
+            if self._is_safe(valid_numbers):
+                return True
+
+        return False
+
+    def _is_valid(self, numbers: list[int]) -> bool:
         for i in range(len(numbers) - 1):
             diff = abs(numbers[i] - numbers[i + 1])
             if diff < 1 or diff > 3:
