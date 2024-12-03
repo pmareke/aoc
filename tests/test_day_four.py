fbffa9b40c2f2c1541b656798c3818209011ebea
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-03 08:49:18 +0100

    feat: prepare day four

---
+++ tests/test_day_four.py
@@ -0,0 +1,42 @@
+import pytest
+from expects import equal, expect
+
+from src.day_four import DayFour
+
+
+class TestDayFour:
+    @staticmethod
+    def example() -> list[str]:
+        return open("inputs/04.example").read().strip().split("\n")
+
+    @staticmethod
+    def real_input() -> list[str]:
+        return open("inputs/04.in").read().strip().split("\n")
+
+    @pytest.mark.parametrize(
+        "input, solution",
+        [
+            (example(), 0),
+            (real_input(), 0),
+        ],
+    )
+    def test_part_one(self, input: list[str], solution: int) -> None:
+        day_one = DayFour(input)
+
+        result = day_one.part_one()
+
+        expect(result).to(equal(solution))
+
+    @pytest.mark.parametrize(
+        "input, solution",
+        [
+            (example(), 0),
+            (real_input(), 0),
+        ],
+    )
+    def test_part_two(self, input: list[str], solution: int) -> None:
+        day_one = DayFour(input)
+
+        result = day_one.part_two()
+
+        expect(result).to(equal(solution))
