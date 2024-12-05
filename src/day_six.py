4d7c6caa14f4a7d04fc185f32df908b4f198eaad
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-05 10:11:52 +0100

    feat: prepare day six

---
+++ src/day_six.py
@@ -0,0 +1,11 @@
+class DaySix:
+    def __init__(self, input: str) -> None:
+        self.inpuit = input
+
+    def part_one(self) -> int:
+        result = 0
+        return result
+
+    def part_two(self) -> int:
+        result = 0
+        return result
