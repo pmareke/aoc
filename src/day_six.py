6af8f6218c3de5e523004da4891071530ba0dd8f
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-06 12:38:21 +0100

    feat: refactor day six

--- src/day_six.py
+++ src/day_six.py
@@ -6,12 +6,12 @@ class DaySix:
         rows = len(self.maze)
         columns = len(self.maze[0])
         direction = 0
-        point_x, point_y = self._find_start()
+        point_x, point_y = self._find_starting_point()
         seen = set()
         while True:
             seen.add((point_x, point_y))
 
-            dx, dy = self._change_direction(direction)
+            dx, dy = self.direction_points(direction)
             new_point_x, new_point_y = [point_x + dx, point_y + dy]
 
             if not (0 <= new_point_x < rows and 0 <= new_point_y < columns):
@@ -22,7 +22,6 @@ class DaySix:
                 continue
 
             point_x, point_y = new_point_x, new_point_y
-
         return len(seen)
 
     def part_two(self) -> int:
@@ -31,7 +30,7 @@ class DaySix:
         loops = 0
         for idx in range(rows):
             for idy in range(columns):
-                point_x, point_y = self._find_start()
+                point_x, point_y = self._find_starting_point()
                 direction = 0
                 SEEN = set()
                 while True:
@@ -41,7 +40,7 @@ class DaySix:
 
                     SEEN.add((direction, point_x, point_y))
 
-                    dx, dy = self._change_direction(direction)
+                    dx, dy = self.direction_points(direction)
                     new_point_x, new_point_y = [point_x + dx, point_y + dy]
 
                     if not (0 <= new_point_x < rows and 0 <= new_point_y < columns):
@@ -58,12 +57,12 @@ class DaySix:
                     point_x, point_y = new_point_x, new_point_y
         return loops
 
-    def _find_start(self) -> list[int]:
+    def _find_starting_point(self) -> list[int]:
         for idx, line in enumerate(self.maze):
             for idy, position in enumerate(line):
                 if position == "^":
                     return [idx, idy]
         raise Exception("No starting point found")
 
-    def _change_direction(self, direction: int) -> tuple[int, int]:
+    def direction_points(self, direction: int) -> tuple[int, int]:
         return [(-1, 0), (0, 1), (1, 0), (0, -1)][direction]
