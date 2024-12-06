d924163063a165a158c8061000661f886f99731b
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-06 12:05:03 +0100

    feat: refactor day six

--- src/day_six.py
+++ src/day_six.py
@@ -1,6 +1,5 @@
 class DaySix:
     def __init__(self, input: str) -> None:
-        self.input = input
         self.maze = input.split("\n")
 
     def part_one(self) -> int:
@@ -48,10 +47,11 @@ class DaySix:
                     if not (0 <= new_point_x < rows and 0 <= new_point_y < columns):
                         break
 
-                    if self.maze[new_point_x][new_point_y] == "#" or [
-                        new_point_x,
-                        new_point_y,
-                    ] == [idx, idy]:
+                    if self.maze[new_point_x][new_point_y] == "#":
+                        direction = (direction + 1) % 4
+                        continue
+
+                    if [new_point_x, new_point_y] == [idx, idy]:
                         direction = (direction + 1) % 4
                         continue
 
@@ -59,15 +59,10 @@ class DaySix:
         return loops
 
     def _find_start(self) -> list[int]:
-        for idx, line in enumerate(self.input.split("\n")):
+        for idx, line in enumerate(self.maze):
             for idy, position in enumerate(line):
-                if position not in [".", "#"]:
+                if position == "^":
                     return [idx, idy]
 
     def _change_direction(self, direction: int) -> tuple[int, int]:
-        return [
-            (-1, 0),
-            (0, 1),
-            (1, 0),
-            (0, -1),
-        ][direction]
+        return [(-1, 0), (0, 1), (1, 0), (0, -1)][direction]
