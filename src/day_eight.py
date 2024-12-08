27e2d2d6a1007407fa1c0e63ee3686850610d1aa
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-08 10:52:36 +0100

    feat: refactor day eight

--- src/day_eight.py
+++ src/day_eight.py
@@ -9,7 +9,6 @@ class DayEight:
             for idy, row in enumerate(column):
                 if row == ".":
                     continue
-
                 _antinodes = self._find_antinodes(antennas, idx, idy)
                 antinodes.update(_antinodes)
         return len(antinodes)
@@ -21,7 +20,6 @@ class DayEight:
             for idy, row in enumerate(column):
                 if row == ".":
                     continue
-
                 _antinodes = self._find_extra_antinodes(antennas, idx, idy)
                 antinodes.update(_antinodes)
         return len(antinodes)
@@ -32,54 +30,51 @@ class DayEight:
             antennas.append(list(line))
         return antennas
 
-    def _find_antinodes(self, antennas: list, x: int, y: int) -> list:
+    def _find_antinodes(self, antennas: list, point_x: int, point_y: int) -> list:
         antinodes = []
-        d = [-1, 0, 1]
-        for dd in d:
-            for idx in range(len(antennas) - (x * dd)):
-                for idy in range(len(antennas[0]) - (y * dd)):
-                    if x == idx and y == idy:
+        movements = [-1, 0, 1]
+        for movement in movements:
+            for idx in range(len(antennas) - (point_x * movement)):
+                for idy in range(len(antennas[0]) - (point_y * movement)):
+                    if point_x == idx and point_y == idy:
                         continue
                     if idx < 0 or idy < 0:
                         continue
                     if idx >= len(antennas) or idy >= len(antennas[idx]):
                         continue
-                    if antennas[x][y] == antennas[idx][idy]:
-                        dx = idx - x
-                        dy = idy - y
-                        xx = x - dx
-                        yy = y - dy
-                        if xx > len(antennas) - 1 or yy > len(antennas[0]) - 1:
+                    if antennas[point_x][point_y] == antennas[idx][idy]:
+                        dx = (2 * point_x) - idx
+                        dy = (2 * point_y) - idy
+                        if dx > len(antennas) - 1 or dy > len(antennas[0]) - 1:
                             continue
-                        if xx < 0 or yy < 0:
+                        if dx < 0 or dy < 0:
                             continue
-                        antinodes.append((xx, yy))
+                        antinodes.append((dx, dy))
         return antinodes
 
-    def _find_extra_antinodes(self, antennas: list, x: int, y: int) -> list:
+    def _find_extra_antinodes(self, antennas: list, point_x: int, point_y: int) -> list:
         antinodes = []
-        d = [-1, 0, 1]
-        for dd in d:
-            for idx in range(len(antennas) - (x * dd)):
-                for idy in range(len(antennas[0]) - (y * dd)):
-                    if x == idx and y == idy:
+        movements = [-1, 0, 1]
+        for movement in movements:
+            for idx in range(len(antennas) - (point_x * movement)):
+                for idy in range(len(antennas[0]) - (point_y * movement)):
+                    if point_x == idx and point_y == idy:
                         continue
                     if idx < 0 or idy < 0:
                         continue
                     if idx >= len(antennas) or idy >= len(antennas[idx]):
                         continue
-                    if antennas[x][y] == antennas[idx][idy]:
-                        multipler = 0
-                        dx = idx - x
-                        dy = idy - y
+                    if antennas[point_x][point_y] == antennas[idx][idy]:
+                        delta = 0
+                        dx = idx - point_x
+                        dy = idy - point_y
                         while True:
-                            xx = x - (dx * multipler)
-                            yy = y - (dy * multipler)
-
+                            xx = point_x - (dx * delta)
+                            yy = point_y - (dy * delta)
                             if xx > len(antennas) - 1 or yy > len(antennas[0]) - 1:
                                 break
                             if xx < 0 or yy < 0:
                                 break
                             antinodes.append((xx, yy))
-                            multipler += 1
+                            delta += 1
         return antinodes
