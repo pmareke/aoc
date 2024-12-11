7f24f47dfc971e6eaf94668e271bf1fd684c3af2
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-11 10:33:06 +0100

    feat: prepare day twelve

---
+++ src/day_twelve.py
@@ -0,0 +1,11 @@
+class DayTwelve:
+    def __init__(self, input: str) -> None:
+        self.input = input
+
+    def part_one(self) -> int:
+        result = 0
+        return result
+
+    def part_two(self) -> int:
+        result = 0
+        return result
