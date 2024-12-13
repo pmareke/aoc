e3ca9c2a8028e625bfdbdc82ad8ee3697ff1521c
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-13 09:46:40 +0100

    feat: add day fourteen

---
+++ src/day_fourteen.py
@@ -0,0 +1,9 @@
+class DayFourteen:
+    def __init__(self, input: str) -> None:
+        self.input = input
+
+    def part_one(self) -> int:
+        return 0
+
+    def part_two(self) -> int:
+        return 0
---
+++ tests/test_day_fourteen.py
@@ -0,0 +1,42 @@
+import pytest
+from expects import equal, expect
+
+from src.day_fourteen import DayFourteen
+
+
+class TestDayFourteen:
+    @staticmethod
+    def example() -> str:
+        return open("inputs/14.example").read().strip()
+
+    @staticmethod
+    def real_input() -> str:
+        return open("inputs/14.in").read().strip()
+
+    @pytest.mark.parametrize(
+        "input, solution",
+        [
+            (example(), 0),
+            (real_input(), 0),
+        ],
+    )
+    def test_part_one(self, input: str, solution: int) -> None:
+        day_one = DayFourteen(input)
+
+        result = day_one.part_one()
+
+        expect(result).to(equal(solution))
+
+    @pytest.mark.parametrize(
+        "input, solution",
+        [
+            (example(), 0),
+            (real_input(), 0),
+        ],
+    )
+    def test_part_two(self, input: str, solution: int) -> None:
+        day_one = DayFourteen(input)
+
+        result = day_one.part_two()
+
+        expect(result).to(equal(solution))
