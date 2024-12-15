5447a5ab85115cea9821a5494bb08d60d3377c56
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-15 11:32:50 +0100

    feat: prepare day sixteen

---
+++ tests/test_day_sixteen.py
@@ -0,0 +1,42 @@
+import pytest
+from expects import equal, expect
+
+from src.day_sixteen import DaySixteen
+
+
+class TestDaySixteen:
+    @staticmethod
+    def example() -> str:
+        return open("inputs/16.example").read().strip()
+
+    @staticmethod
+    def real_input() -> str:
+        return open("inputs/16.in").read().strip()
+
+    @pytest.mark.parametrize(
+        "input, solution",
+        [
+            (example(), 0),
+            (real_input(), 0),
+        ],
+    )
+    def test_part_one(self, input: str, solution: int) -> None:
+        day_one = DaySixteen(input)
+
+        result = day_one.part_one()
+
+        expect(result).to(equal(solution))
+
+    @pytest.mark.parametrize(
+        "input, solution",
+        [
+            (example(), 0),
+            (real_input(), 0),
+        ],
+    )
+    def test_part_two(self, input: str, solution: int) -> None:
+        day_one = DaySixteen(input)
+
+        result = day_one.part_two()
+
+        expect(result).to(equal(solution))
