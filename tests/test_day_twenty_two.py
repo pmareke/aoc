d4eaf0b9da0d4e46d956e923aed3c0159ca132b2
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-21 21:43:25 +0100

    feat: prepare day twenty two

---
+++ tests/test_day_twenty_two.py
@@ -0,0 +1,42 @@
+import pytest
+from expects import equal, expect
+
+from src.day_twenty_two import DayTwentyTwo
+
+
+class TestDayTwentyTwo:
+    @staticmethod
+    def example() -> str:
+        return open("inputs/22.example").read().strip()
+
+    @staticmethod
+    def real_input() -> str:
+        return open("inputs/22.in").read().strip()
+
+    @pytest.mark.parametrize(
+        "input, solution",
+        [
+            (example(), 0),
+            (real_input(), 0),
+        ],
+    )
+    def test_part_one(self, input: str, solution: int) -> None:
+        day_one = DayTwentyTwo(input)
+
+        result = day_one.part_one()
+
+        expect(result).to(equal(solution))
+
+    @pytest.mark.parametrize(
+        "input, solution",
+        [
+            (example(), 0),
+            (real_input(), 0),
+        ],
+    )
+    def test_part_two(self, input: str, solution: int) -> None:
+        day_one = DayTwentyTwo(input)
+
+        result = day_one.part_two()
+
+        expect(result).to(equal(solution))
