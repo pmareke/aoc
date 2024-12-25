405c89b9c71c5b4b992d029888b758652e02a0a9
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-12-25 11:25:53 +0100

    feat: refactor day twenty five

--- src/day_twenty_five.py
+++ src/day_twenty_five.py
@@ -9,32 +9,23 @@ class DayTwentyFive:
             for key in keys:
                 overlapping = False
                 for idx in range(len(lock)):
-                    lock_weight = lock[idx]
-                    key_weight = len(key) - key[idx] + 1
-                    if lock_weight >= key_weight:
-                        overlapping = True
+                    for idy in range(len(lock[idx])):
+                        _lock = lock[idx][idy]
+                        _key = key[idx][idy]
+                        if _lock == "#" and _key == "#":
+                            overlapping = True
                 if not overlapping:
                     total += 1
         return total
 
-    def _parse_input(self) -> tuple[list[list[int]], list[list[int]]]:
+    def _parse_input(self) -> tuple[list[list[str]], list[list[str]]]:
         locks = []
         keys = []
         for blocks in self.input.split("\n\n"):
             block = blocks.split("\n")
-            is_block = False
-            if block[0] == "#" * len(block[0]):
-                is_block = True
-            weights = [0] * len(block[0])
-            for idx in range(1, len(block) - 1):
-                for idy, char in enumerate(block[idx]):
-                    if is_block:
-                        weights[idy] = idx if char == "#" else weights[idy]
-                    if weights[idy] == 0:
-                        position = len(block) - idx - 1
-                        weights[idy] = position if char == "#" else weights[idy]
-            if is_block:
-                locks.append(weights)
+            chars = [row for row in block]
+            if block[0][0] == "#":
+                locks.append(chars)
                 continue
-            keys.append(weights)
+            keys.append(chars)
         return locks, keys
